8d008d1e997f4b61ed5fc23c48393f5cef3f9cfa
Mohit Sharma <mohitsharma@Mohits-MacBook-Pro.local>
2023-04-11 19:40:12 +0530

    fixing circleci error

--- build-pkg.sh
+++ build-pkg.sh
@@ -5,6 +5,7 @@ mkdir pkg
 cp -r bin lib handler.py question.py utils.py requirements.txt pkg/
 cd pkg
 cd lib 
+sudo apt update
 apt download libglib2.0-0 libnss3 libgconf-2-4 libfontconfig1
 cd ..
 docker run -v "$PWD":/var/task "lambci/lambda:build-python3.7" /bin/sh -c 'pip install -r requirements.txt -t . --no-cache-dir --compile; exit'
